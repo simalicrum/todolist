09eb3b1456cb291762a834cb1ebbdcc8337cdbf8
Robert Reinert <robert.reinert@gmail.com>
2020-09-13 16:28:43 -0700

    Blocking renderitem

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log("Hello, World");
+
+const toDoItem = (title, description, dueDate, priority) => {
+  return {title, description, dueDate, priority}
+}
+
+let vacuuming = toDoItem("Vacuum the house", "Vacuuming the house means that you get out the vacuum cleaner and plug it in and push it around the floor.", "November 5, 2020", "High");
+
+console.log(vacuuming);

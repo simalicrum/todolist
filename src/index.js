32dbf1fc3bc838bafb7ea1da77b9bae70fe7747c
Robert Reinert <robert.reinert@gmail.com>
2020-09-16 20:20:57 -0700

    Linter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { toDoItem, Projects, toDoList } from "./todoLogic.js";
+import { toDoItem, Projects, toDoList } from './todoLogic';
 import {
   renderToDoItem,
   renderAddForm,
@@ -8,57 +8,59 @@ import {
   renderProject,
   renderListOfProjects,
   renderProjectForm,
-} from "./todoDOM.js";
+} from './todoDOM';
 
-let myStorage = window.localStorage;
+const projects = Projects();
 
-let importProjects = JSON.parse(myStorage.getItem("projects"));
+const myStorage = window.localStorage;
+
+const importProjects = JSON.parse(myStorage.getItem('projects'));
 
 function returnTaskItem(uniqueID) {
-  let thing = projects.list.find(
-    (element) => element.getToDoItemIndexfromUniqueID(uniqueID) != -1
+  const thing = projects.list.find(
+    (element) => element.getToDoItemIndexfromUniqueID(uniqueID) !== -1
   );
   return thing.list[thing.getToDoItemIndexfromUniqueID(uniqueID)];
 }
 
 function addDeleteButtonLogic(uniqueID) {
   document
-    .getElementById(uniqueID + "-delete-button")
-    .addEventListener("click", function () {
+    .getElementById(uniqueID + '-delete-button')
+    .addEventListener('click', function () {
       projects.list[
         projects.getTaskListIndexfromUniqueID(
-          document.querySelector(".shown-list").id.slice(0, -8)
+          document.querySelector('.shown-list').id.slice(0, -8)
         )
       ].removeToDoItem(uniqueID);
     });
 }
 
 function addDoneToggleLogic(uniqueID) {
-  let toggleElement = document.getElementById(uniqueID + "-toggle-done");
-  let taskElement = document.getElementById(uniqueID + "-task");
-  toggleElement.addEventListener("click", function () {
+  const toggleElement = document.getElementById(uniqueID + '-toggle-done');
+  const taskElement = document.getElementById(uniqueID + '-task');
+  toggleElement.addEventListener('click', function () {
     let taskItemFound = returnTaskItem(uniqueID);
     if (toggleElement.checked == true) {
       taskItemFound.done = true;
-      taskElement.style.textDecoration = "line-through";
+      taskElement.style.textDecoration = 'line-through';
     } else {
       taskItemFound.done = false;
-      taskElement.style.textDecoration = "";
+      taskElement.style.textDecoration = '';
     }
   });
 }
 
 function addPriorityToggleLogic(uniqueID) {
-  let priorityElement = document.getElementById(uniqueID + "-priority");
+  let priorityElement = document.getElementById(uniqueID + '-priority');
   let taskItemFound = returnTaskItem(uniqueID);
   priorityElement.value = taskItemFound.priority;
-  priorityElement.addEventListener("change", function () {
+  priorityElement.addEventListener('change', function () {
     let taskItemFound = returnTaskItem(uniqueID);
     taskItemFound.priority = priorityElement.value;
   });
 }
 
-let projects = Projects();
+
 
 // Import projects from localStorage starts here
 
@@ -89,129 +91,129 @@ if (importProjects != null) {
 // Add demo projects and tasks
 
 if (projects.list.length == 0) {
-  projects.addToDoList(toDoList("Default", projects.generateUniqueID()));
+  projects.addToDoList(toDoList('Default', projects.generateUniqueID()));
   projects.addToDoList(
-    toDoList("Build a Rocket Ship", projects.generateUniqueID())
+    toDoList('Build a Rocket Ship', projects.generateUniqueID())
   );
 
-  projects.list[projects.getTaskListIndex("Default")].addToDoItem(
+  projects.list[projects.getTaskListIndex('Default')].addToDoItem(
     toDoItem(
-      "Vacuum the house",
-      "Vacuuming the house means that you get out the vacuum cleaner and plug it in and push it around the floor.",
-      "November 5, 2020",
-      "medium",
+      'Vacuum the house',
+      'Vacuuming the house means that you get out the vacuum cleaner and plug it in and push it around the floor.',
+      'November 5, 2020',
+      'medium',
       false,
-      projects.list[projects.getTaskListIndex("Default")].generateUniqueID()
+      projects.list[projects.getTaskListIndex('Default')].generateUniqueID()
     )
   );
 
-  projects.list[projects.getTaskListIndex("Default")].addToDoItem(
+  projects.list[projects.getTaskListIndex('Default')].addToDoItem(
     toDoItem(
-      "Do the dishes",
-      "Put the dishes in the dishwasher and turn it on. Then put the clean dishes into the cupboards.",
-      "September 10, 2020",
-      "medium",
+      'Do the dishes',
+      'Put the dishes in the dishwasher and turn it on. Then put the clean dishes into the cupboards.',
+      'September 10, 2020',
+      'medium',
       false,
-      projects.list[projects.getTaskListIndex("Default")].generateUniqueID()
+      projects.list[projects.getTaskListIndex('Default')].generateUniqueID()
     )
   );
 
-  projects.list[projects.getTaskListIndex("Build a Rocket Ship")].addToDoItem(
+  projects.list[projects.getTaskListIndex('Build a Rocket Ship')].addToDoItem(
     toDoItem(
-      "Hire Astronauts",
-      "Check out indeed.com for good candidates.",
-      "January 1, 2025",
-      "low",
+      'Hire Astronauts',
+      'Check out indeed.com for good candidates.',
+      'January 1, 2025',
+      'low',
       false,
       projects.list[
-        projects.getTaskListIndex("Build a Rocket Ship")
+        projects.getTaskListIndex('Build a Rocket Ship')
       ].generateUniqueID()
     )
   );
 
-  projects.list[projects.getTaskListIndex("Build a Rocket Ship")].addToDoItem(
+  projects.list[projects.getTaskListIndex('Build a Rocket Ship')].addToDoItem(
     toDoItem(
-      "Buy Engines",
-      "Buy the best thing on amazon.",
-      "Febuary 15, 2023",
-      "low",
+      'Buy Engines',
+      'Buy the best thing on amazon.',
+      'Febuary 15, 2023',
+      'low',
       false,
       projects.list[
-        projects.getTaskListIndex("Build a Rocket Ship")
+        projects.getTaskListIndex('Build a Rocket Ship')
       ].generateUniqueID()
     )
   );
 }
-renderListOfProjects("lists", projects);
+renderListOfProjects('lists', projects);
 
-renderProjectForm("new-project-ok", "new-project-form");
+renderProjectForm('new-project-ok', 'new-project-form');
 
 document
-  .getElementById("new-project-button")
-  .addEventListener("click", function () {
-    document.getElementById("new-project-button").style.display = "none";
-    showForm("new-project-form");
+  .getElementById('new-project-button')
+  .addEventListener('click', function () {
+    document.getElementById('new-project-button').style.display = 'none';
+    showForm('new-project-form');
   });
 
 document
-  .getElementById("new-project-ok")
-  .addEventListener("click", function () {
+  .getElementById('new-project-ok')
+  .addEventListener('click', function () {
     projects.addToDoList(
       toDoList(
-        document.getElementById("project-name").value,
+        document.getElementById('project-name').value,
         projects.generateUniqueID()
       )
     );
-    document.getElementById("project-name").value = "";
-    renderProject("lists", projects.list[projects.list.length - 1]);
-    renderTaskList("content", projects.list[projects.list.length - 1]);
-    document.getElementById("new-project-button").style.display = "block";
-    hideForm("new-project-form");
+    document.getElementById('project-name').value = '';
+    renderProject('lists', projects.list[projects.list.length - 1]);
+    renderTaskList('content', projects.list[projects.list.length - 1]);
+    document.getElementById('new-project-button').style.display = 'block';
+    hideForm('new-project-form');
   });
 
-renderAddForm("add-task-ok", "add-task-form");
+renderAddForm('add-task-ok', 'add-task-form');
 
 document
-  .getElementById("add-task-button")
-  .addEventListener("click", function () {
-    document.getElementById("add-task-button").style.display = "none";
-    showForm("add-task-form");
+  .getElementById('add-task-button')
+  .addEventListener('click', function () {
+    document.getElementById('add-task-button').style.display = 'none';
+    showForm('add-task-form');
   });
 
-document.getElementById("add-task-ok").addEventListener("click", function () {
+document.getElementById('add-task-ok').addEventListener('click', function () {
   let newUniqueID = projects.list[
     projects.getTaskListIndexfromUniqueID(
-      document.querySelector(".shown-list").id.slice(0, -8)
+      document.querySelector('.shown-list').id.slice(0, -8)
     )
   ].generateUniqueID();
   projects.list[
     projects.getTaskListIndexfromUniqueID(
-      document.querySelector(".shown-list").id.slice(0, -8)
+      document.querySelector('.shown-list').id.slice(0, -8)
     )
   ].addToDoItem(
     toDoItem(
-      document.getElementById("form-title").value,
-      document.getElementById("form-description").value,
-      document.getElementById("form-date").value,
-      document.getElementById("form-priority").value,
+      document.getElementById('form-title').value,
+      document.getElementById('form-description').value,
+      document.getElementById('form-date').value,
+      document.getElementById('form-priority').value,
       false,
       newUniqueID
     )
   );
-  document.getElementById("form-title").value = "";
-  document.getElementById("form-description").value = "";
-  document.getElementById("form-date").value = "";
-  document.getElementById("form-priority").value = "";
+  document.getElementById('form-title').value = '';
+  document.getElementById('form-description').value = '';
+  document.getElementById('form-date').value = '';
+  document.getElementById('form-priority').value = '';
   renderToDoItem(
-    document.querySelector(".shown-list").id,
+    document.querySelector('.shown-list').id,
     projects.list[
       projects.getTaskListIndexfromUniqueID(
-        document.querySelector(".shown-list").id.slice(0, -8)
+        document.querySelector('.shown-list').id.slice(0, -8)
       )
     ].list[
       projects.list[
         projects.getTaskListIndexfromUniqueID(
-          document.querySelector(".shown-list").id.slice(0, -8)
+          document.querySelector('.shown-list').id.slice(0, -8)
         )
       ].list.length - 1
     ]
@@ -219,16 +221,16 @@ document.getElementById("add-task-ok").addEventListener("click", function () {
   addDeleteButtonLogic(newUniqueID);
   addDoneToggleLogic(newUniqueID);
   addPriorityToggleLogic(newUniqueID);
-  document.getElementById("add-task-button").style.display = "block";
-  hideForm("add-task-form");
+  document.getElementById('add-task-button').style.display = 'block';
+  hideForm('add-task-form');
 });
 
-projects.list.forEach((element) => renderTaskList("content", element));
+projects.list.forEach((element) => renderTaskList('content', element));
 document
   .getElementById(
-    projects.list[projects.getTaskListIndex("Default")].uniqueID + "-content"
+    projects.list[projects.getTaskListIndex('Default')].uniqueID + '-content'
   )
-  .setAttribute("class", "shown-list");
+  .setAttribute('class', 'shown-list');
 
 // add button logic to demo entries
 
@@ -240,7 +242,7 @@ projects.list.forEach((i) => {
   });
 });
 
-window.addEventListener("beforeunload", function () {
-  myStorage.removeItem("projects");
-  myStorage.setItem("projects", JSON.stringify(projects));
+window.addEventListener('beforeunload', function () {
+  myStorage.removeItem('projects');
+  myStorage.setItem('projects', JSON.stringify(projects));
 });
--- src/todoDOM.js
+++ src/todoDOM.js
@@ -1,43 +1,43 @@
 const renderToDoItem = (element, toDoItemObj) => {
-  let content = document.getElementById(element);
-  let todoItem = document.createElement("div");
-  todoItem.setAttribute("id", toDoItemObj.uniqueID + "-task");
-  todoItem.setAttribute("class", "to-do-item");
-  let todoItemToggle = document.createElement("input");
-  todoItemToggle.setAttribute("type", "checkbox");
-  todoItemToggle.setAttribute("id", toDoItemObj.uniqueID + "-toggle-done");
-  todoItemToggle.setAttribute("name", toDoItemObj.uniqueID + "-toggle-done");
-  let todoItemTitle = document.createElement("label");
-  todoItemTitle.setAttribute("class", "to-do-item-title");
-  todoItemTitle.setAttribute("for", toDoItemObj.uniqueID + "-toggle-done");
+  const content = document.getElementById(element);
+  const todoItem = document.createElement('div');
+  todoItem.setAttribute('id', `${toDoItemObj.uniqueID}-task`);
+  todoItem.setAttribute('class', 'to-do-item');
+  const todoItemToggle = document.createElement('input');
+  todoItemToggle.setAttribute('type', 'checkbox');
+  todoItemToggle.setAttribute('id', toDoItemObj.uniqueID + '-toggle-done');
+  todoItemToggle.setAttribute('name', toDoItemObj.uniqueID + '-toggle-done');
+  const todoItemTitle = document.createElement('label');
+  todoItemTitle.setAttribute('class', 'to-do-item-title');
+  todoItemTitle.setAttribute('for', toDoItemObj.uniqueID + '-toggle-done');
   todoItemTitle.innerHTML = toDoItemObj.title;
-  let todoItemDesc = document.createElement("p");
-  todoItemDesc.setAttribute("class", "to-do-item-desc");
+  const todoItemDesc = document.createElement('p');
+  todoItemDesc.setAttribute('class', 'to-do-item-desc');
   todoItemDesc.innerHTML = toDoItemObj.description;
-  let todoItemDate = document.createElement("div");
-  todoItemDate.innerHTML = "Due Date: " + toDoItemObj.dueDate;
-  let todoItemPriority = document.createElement("select");
-  todoItemPriority.setAttribute("name", toDoItemObj.uniqueID + "-priority");
-  todoItemPriority.setAttribute("id", toDoItemObj.uniqueID + "-priority");
-  let priorityHigh = document.createElement("option");
-  priorityHigh.setAttribute("value", "high");
-  priorityHigh.innerHTML = "High";
-  let priorityMedium = document.createElement("option");
-  priorityMedium.setAttribute("value", "medium");
-  priorityMedium.innerHTML = "Medium";
-  let priorityLow = document.createElement("option");
-  priorityLow.setAttribute("value", "low");
-  priorityLow.innerHTML = "Low";
-  let todoDelete = document.createElement("button");
-  todoDelete.setAttribute("id", toDoItemObj.uniqueID + "-delete-button");
-  todoDelete.innerHTML = "Delete Task";
-  todoDelete.addEventListener("click", function () {
+  const todoItemDate = document.createElement('div');
+  todoItemDate.innerHTML = 'Due Date: ' + toDoItemObj.dueDate;
+  const todoItemPriority = document.createElement('select');
+  todoItemPriority.setAttribute('name', toDoItemObj.uniqueID + '-priority');
+  todoItemPriority.setAttribute('id', toDoItemObj.uniqueID + '-priority');
+  const priorityHigh = document.createElement('option');
+  priorityHigh.setAttribute('value', 'high');
+  priorityHigh.innerHTML = 'High';
+  const priorityMedium = document.createElement('option');
+  priorityMedium.setAttribute('value', 'medium');
+  priorityMedium.innerHTML = 'Medium';
+  const priorityLow = document.createElement('option');
+  priorityLow.setAttribute('value', 'low');
+  priorityLow.innerHTML = 'Low';
+  const todoDelete = document.createElement('button');
+  todoDelete.setAttribute('id', toDoItemObj.uniqueID + '-delete-button');
+  todoDelete.innerHTML = 'Delete Task';
+  todoDelete.addEventListener('click', function () {
     document
-      .querySelector(".shown-list")
-      .removeChild(document.getElementById(toDoItemObj.uniqueID + "-task"));
+      .querySelector('.shown-list')
+      .removeChild(document.getElementById(toDoItemObj.uniqueID + '-task'));
   });
   if (toDoItemObj.done == true) {
-    todoItem.style.textDecoration = "line-through";
+    todoItem.style.textDecoration = 'line-through';
     todoItemToggle.checked = true;
   }
   todoItemPriority.appendChild(priorityHigh);
@@ -47,105 +47,105 @@ const renderToDoItem = (element, toDoItemObj) => {
   todoItem.appendChild(todoItemTitle);
   todoItem.appendChild(todoItemDesc);
   todoItem.appendChild(todoItemDate);
-  todoItem.appendChild(document.createElement("br"));
+  todoItem.appendChild(document.createElement('br'));
   todoItem.appendChild(todoItemPriority);
-  todoItem.appendChild(document.createElement("br"));
-  todoItem.appendChild(document.createElement("br"));
+  todoItem.appendChild(document.createElement('br'));
+  todoItem.appendChild(document.createElement('br'));
   todoItem.appendChild(todoDelete);
   content.appendChild(todoItem);
   
 };
 
 const renderAddForm = (okButton, formTarget) => {
-  let target = document.getElementById(formTarget);
-  let todoNewItemForm = document.createElement("form");
-  let todoItemTitle = document.createElement("input");
-  todoItemTitle.setAttribute("id", "form-title");
-  let todoItemTitleLabel = document.createElement("label");
-  todoItemTitleLabel.innerHTML = "Title";
-  let todoItemDescription = document.createElement("input");
-  todoItemDescription.setAttribute("id", "form-description");
-  let todoItemDescriptionLabel = document.createElement("label");
-  todoItemDescriptionLabel.innerHTML = "Description";
-  let todoItemDate = document.createElement("input");
-  todoItemDate.setAttribute("type", "date");
-  todoItemDate.setAttribute("id", "form-date");
-  let todoItemDateLabel = document.createElement("label");
-  todoItemDateLabel.innerHTML = "Due Date";
-  let todoItemPriority = document.createElement("select");
-  todoItemPriority.setAttribute("id", "form-priority");
-  todoItemPriority.setAttribute("name", "priority");
-  let todoItemPriorityLabel = document.createElement("label");
-  todoItemPriorityLabel.innerHTML = "Priority:";
-  let priorityHigh = document.createElement("option");
-  priorityHigh.setAttribute("value", "high");
-  priorityHigh.innerHTML = "High";
-  let priorityMedium = document.createElement("option");
-  priorityMedium.setAttribute("value", "medium");
-  priorityMedium.innerHTML = "Medium";
-  let priorityLow = document.createElement("option");
-  priorityLow.setAttribute("value", "low");
-  priorityLow.innerHTML = "Low";
-  let todoItemAdd = document.createElement("button");
-  todoItemAdd.setAttribute("id", okButton);
-  todoItemAdd.innerHTML = "Add";
+  const target = document.getElementById(formTarget);
+  const todoNewItemForm = document.createElement('form');
+  const todoItemTitle = document.createElement('input');
+  todoItemTitle.setAttribute('id', 'form-title');
+  const todoItemTitleLabel = document.createElement('label');
+  todoItemTitleLabel.innerHTML = 'Title';
+  const todoItemDescription = document.createElement('input');
+  todoItemDescription.setAttribute('id', 'form-description');
+  const todoItemDescriptionLabel = document.createElement('label');
+  todoItemDescriptionLabel.innerHTML = 'Description';
+  const todoItemDate = document.createElement('input');
+  todoItemDate.setAttribute('type', 'date');
+  todoItemDate.setAttribute('id', 'form-date');
+  const todoItemDateLabel = document.createElement('label');
+  todoItemDateLabel.innerHTML = 'Due Date';
+  const todoItemPriority = document.createElement('select');
+  todoItemPriority.setAttribute('id', 'form-priority');
+  todoItemPriority.setAttribute('name', 'priority');
+  const todoItemPriorityLabel = document.createElement('label');
+  todoItemPriorityLabel.innerHTML = 'Priority:';
+  const priorityHigh = document.createElement('option');
+  priorityHigh.setAttribute('value', 'high');
+  priorityHigh.innerHTML = 'High';
+  const priorityMedium = document.createElement('option');
+  priorityMedium.setAttribute('value', 'medium');
+  priorityMedium.innerHTML = 'Medium';
+  const priorityLow = document.createElement('option');
+  priorityLow.setAttribute('value', 'low');
+  priorityLow.innerHTML = 'Low';
+  const todoItemAdd = document.createElement('button');
+  todoItemAdd.setAttribute('id', okButton);
+  todoItemAdd.innerHTML = 'Add';
   todoNewItemForm.appendChild(todoItemTitleLabel);
-  todoNewItemForm.appendChild(document.createElement("br"));
+  todoNewItemForm.appendChild(document.createElement('br'));
   todoNewItemForm.appendChild(todoItemTitle);
-  todoNewItemForm.appendChild(document.createElement("br"));
+  todoNewItemForm.appendChild(document.createElement('br'));
   todoNewItemForm.appendChild(todoItemDescriptionLabel);
-  todoNewItemForm.appendChild(document.createElement("br"));
+  todoNewItemForm.appendChild(document.createElement('br'));
   todoNewItemForm.appendChild(todoItemDescription);
-  todoNewItemForm.appendChild(document.createElement("br"));
+  todoNewItemForm.appendChild(document.createElement('br'));
   todoNewItemForm.appendChild(todoItemDateLabel);
-  todoNewItemForm.appendChild(document.createElement("br"));
+  todoNewItemForm.appendChild(document.createElement('br'));
   todoNewItemForm.appendChild(todoItemDate);
-  todoNewItemForm.appendChild(document.createElement("br"));
+  todoNewItemForm.appendChild(document.createElement('br'));
   todoNewItemForm.appendChild(todoItemPriorityLabel);
-  todoNewItemForm.appendChild(document.createElement("br"));
+  todoNewItemForm.appendChild(document.createElement('br'));
   todoItemPriority.appendChild(priorityHigh);
   todoItemPriority.appendChild(priorityMedium);
   todoItemPriority.appendChild(priorityLow);
   todoNewItemForm.appendChild(todoItemPriority);
-  todoNewItemForm.appendChild(document.createElement("br"));
+  todoNewItemForm.appendChild(document.createElement('br'));
   target.appendChild(todoNewItemForm);
   target.appendChild(todoItemAdd);
 };
 
 const showForm = (formId) => {
-  document.getElementById(formId).style.display = "block";
+  document.getElementById(formId).style.display = 'block';
 };
 
 const hideForm = (formId) => {
-  document.getElementById(formId).style.display = "none";
+  document.getElementById(formId).style.display = 'none';
 };
 
 const renderTaskList = (element, taskList) => {
   let target = document.getElementById(element);
-  let listDiv = document.createElement("div");
-  listDiv.setAttribute("id", taskList.uniqueID + "-content");
-  listDiv.setAttribute("class", "hidden-list");
+  let listDiv = document.createElement('div');
+  listDiv.setAttribute('id', taskList.uniqueID + '-content');
+  listDiv.setAttribute('class', 'hidden-list');
   target.appendChild(listDiv);
   taskList.list.forEach((i) =>
-    renderToDoItem(taskList.uniqueID + "-content", i)
+    renderToDoItem(taskList.uniqueID + '-content', i)
   );
 };
 
 const renderProject = (element, project) => {
   let target = document.getElementById(element);
-  let projectDiv = document.createElement("div");
+  let projectDiv = document.createElement('div');
   projectDiv.innerHTML = project.name;
-  projectDiv.setAttribute("id", project.uniqueID);
-  projectDiv.setAttribute("class", "project");
-  projectDiv.addEventListener("click", function showTaskList() {
-    document.querySelector(".shown-list").setAttribute("class", "hidden-list");
+  projectDiv.setAttribute('id', project.uniqueID);
+  projectDiv.setAttribute('class', 'project');
+  projectDiv.addEventListener('click', function showTaskList() {
+    document.querySelector('.shown-list').setAttribute('class', 'hidden-list');
     document
-      .getElementById(project.uniqueID + "-content")
-      .setAttribute("class", "shown-list");
-    document.querySelectorAll(".project").forEach((element) => {
-      element.style.background = "white";
+      .getElementById(project.uniqueID + '-content')
+      .setAttribute('class', 'shown-list');
+    document.querySelectorAll('.project').forEach((element) => {
+      element.style.background = 'white';
     });
-    projectDiv.style.background = "lightgrey";
+    projectDiv.style.background = 'lightgrey';
   });
   target.appendChild(projectDiv);
 };
@@ -155,21 +155,21 @@ const renderListOfProjects = (element, projects) => {
     renderProject(element, project);
   });
   document.getElementById(projects.list[0].uniqueID).style.background =
-    "lightgrey";
+    'lightgrey';
 };
 
 const renderProjectForm = (okButton, formTarget) => {
-  let target = document.getElementById(formTarget);
-  let newProjectForm = document.createElement("form");
-  let newProjectName = document.createElement("input");
-  newProjectName.setAttribute("id", "project-name");
-  let newProjectNameLabel = document.createElement("label");
-  newProjectNameLabel.innerHTML = "Project Name:";
-  let newProjectAdd = document.createElement("button");
-  newProjectAdd.setAttribute("id", okButton);
-  newProjectAdd.innerHTML = "Add";
+  const target = document.getElementById(formTarget);
+  const newProjectForm = document.createElement('form');
+  const newProjectName = document.createElement('input');
+  newProjectName.setAttribute('id', 'project-name');
+  const newProjectNameLabel = document.createElement('label');
+  newProjectNameLabel.innerHTML = 'Project Name:';
+  const newProjectAdd = document.createElement('button');
+  newProjectAdd.setAttribute('id', okButton);
+  newProjectAdd.innerHTML = 'Add';
   newProjectForm.appendChild(newProjectNameLabel);
-  newProjectForm.appendChild(document.createElement("br"));
+  newProjectForm.appendChild(document.createElement('br'));
   newProjectForm.appendChild(newProjectName);
   target.appendChild(newProjectForm);
   target.appendChild(newProjectAdd);
